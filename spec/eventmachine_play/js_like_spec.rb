acab4df585d10b2b6018d38c13ed8e93b9ee7fb7
Caleb Buxton <me@cpb.ca>
2011-09-19 17:27:33 -0700

    fixed specs to be EM.schedule sensitive

--- spec/eventmachine_play/js_like_spec.rb
+++ spec/eventmachine_play/js_like_spec.rb
@@ -10,6 +10,8 @@ describe EventmachinePlay::JsLike do
     end.new
   end
 
+  let(:dog_catcher) { double("Dog Catcher") }
+
   context "private methods" do
     it "should only create one channel per event name" do
       example_emitter.send(:channel_for,:event_name).should eql(example_emitter.send(:channel_for,"event_name"))
@@ -28,18 +30,51 @@ describe EventmachinePlay::JsLike do
 
       em do
         example_emitter.add_event(:meow) { |one,two|
-          puts "hello?"
           calls += 1
           args.push([one,two])
           done
         }
 
-        expect {
-          example_emitter.trigger_event(:meow,1,2)
-        }.to change { calls }.by(1)
+        example_emitter.trigger_event(:meow,1,2)
+      end
+      calls.should eql(1)
+      args.first.should eql([1,2])
+    end
+
+    it "allows you to add multiple responders" do
+      calls = 0
+
+      dog_catcher.should_receive(:heard_something).with(1,2).once
+
+      em do
+        example_emitter.add_event(:bark) { |*arg|
+          calls += 1
+        }
+
+        example_emitter.add_event(:bark,dog_catcher,:heard_something)
+
+        example_emitter.add_event(:bark) do |*args|
+          done
+        end
+
+        example_emitter.trigger_event(:bark,1,2)
       end
 
-      args.first.should eql([1,2])
+      calls.should eql(1)
+    end
+
+    it "only triggers the event's responders" do
+      dog_catcher.should_not_receive(:heard_something).with(1,2)
+
+      em do
+        example_emitter.add_event(:bark, dog_catcher, :heard_something)
+
+        example_emitter.add_event(:meow) do |*args|
+          done
+        end
+
+        example_emitter.trigger_event(:meow,"quietly")
+      end
     end
   end
 end
